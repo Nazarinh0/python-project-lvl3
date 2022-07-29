3a8fdd85695d04d36a324d17ab85135ca5100bce
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-29 15:58:32 +0300

    add test_loader function

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,1 +1,13 @@
+import os.path
+
+import requests_mock
+from page_loader import download
+
 def test_loader():
+    with requests_mock.Mocker() as m:
+        page_url = 'http://ru.hexlet.io/courses'
+        m.get(page_url, text = 'example text')
+        expected = 'ru-hexlet-io-courses.html'
+        assert download(page_url) == expected
+        with open(os.path.join(os.getcwd(), expected)) as f:
+            assert f.read() == 'example text'
