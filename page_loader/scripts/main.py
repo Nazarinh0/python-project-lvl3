ea3906a07f3ce25c74d19dc0bfb538a6e8ab5415
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-29 11:56:55 +0300

    add content to main.py

--- page_loader/scripts/main.py
+++ page_loader/scripts/main.py
@@ -1,6 +1,16 @@
-def main():
-    pass
+"""Main script"""
+import argparse
 
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Loads a page from url and saves it to HTML'
+    )
+    parser.add_argument('page_url')
+    parser.add_argument(
+        '--output',
+        help='path to existing directory, in which HTML will be saved')
+    args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
