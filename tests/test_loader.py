cfc262a00c3c0ffd353a9433eab28c601a718f13
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-30 16:59:44 +0300

    split 2 tests in test_loader.py

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,13 +1,24 @@
 import os.path
-
+import tempfile
 import requests_mock
 from page_loader import download
 
-def test_loader():
+
+def test_loader_content():
     with requests_mock.Mocker() as m:
         page_url = 'http://ru.hexlet.io/courses'
         m.get(page_url, text = 'example text')
         expected = 'ru-hexlet-io-courses.html'
-        assert download(page_url) == expected
-        with open(os.path.join(os.getcwd(), expected)) as f:
-            assert f.read() == 'example text'
+        with tempfile.TemporaryDirectory() as tmpdir:
+            assert download(page_url, path=tmpdir) == expected
+            with open(os.path.join(tmpdir, expected)) as f:
+                assert f.read() == 'example text'
+
+
+def test_loader_filename():
+    with requests_mock.Mocker() as m:
+        page_url = 'http://ru.hexlet.io/courses'
+        m.get(page_url, text = 'example text')
+        expected = 'ru-hexlet-io-courses.html'
+        with tempfile.TemporaryDirectory() as tmpdir:
+            assert download(page_url, path=tmpdir) == expected
